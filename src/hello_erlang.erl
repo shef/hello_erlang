0d7dca8f7c265c4cb4340f3f072da5fd909c4931
Aaron Spiegel <Aaron@kapowmedia.net>
2014-06-16 21:58:11 -0500

    Initial Commit

---
+++ src/hello_erlang.erl
@@ -0,0 +1,10 @@
+-module(hello_erlang).
+
+-export([start/0]).
+
+start() ->
+    ok = application:start(crypto),
+    ok = application:start(ranch),
+    ok = application:start(cowlib),
+    ok = application:start(cowboy),
+    ok = application:start(hello_erlang).
